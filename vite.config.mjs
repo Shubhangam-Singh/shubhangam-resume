66a5c030f0341738071049a3b10bfd9f46899200
Shubhangam Singh <shubhangam2005singh@gmail.com>
2025-08-24 18:56:46 +0530

    Setup for GitHub Pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // If you plan to deploy to GitHub Pages at /shubhangam-resume/, set:
-  // base: '/shubhangam-resume/'
+  base: '/shubhangam-resume/' // IMPORTANT: Replace with your repo name
 })
